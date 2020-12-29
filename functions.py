29b90af208a4dc42c3fdb9ba061edbaf23b7b073
unknown <Sho Okumoto>
2020-12-29 14:04:06 +0900

    make functions enable to handle sparseness

--- functions.py
+++ functions.py
@@ -6,12 +6,12 @@ def anistrophic_decrease(X, sigma):
     dim = X.shape[1]
 
     y = np.zeros(num)
-
+    ascending_sigma = np.argsort(sigma)
     normalizer = 0
     phi = np.sqrt(2) * np.cos(2 * np.pi * X)
     for i in range(dim):
-        normalizer = normalizer + 2 ** sigma[i] 
-        y = y + np.prod(phi[:, 0:(i+1)], axis=1) / normalizer
+        normalizer = normalizer + 2 ** sigma[ascending_sigma[i]] 
+        y = y + np.prod(phi[:, ascending_sigma[0:(i+1)]], axis=1) / normalizer
 
     return y    
 
@@ -21,12 +21,12 @@ def mix_decrease(X, sigma):
     dim = X.shape[1]
 
     y = np.zeros(num)
-
+    ascending_sigma = np.argsort(sigma)
     normalizer = 1
     phi = np.sqrt(2) * np.cos(2 * np.pi * X)
     for i in range(dim):
-        normalizer = normalizer * 2 ** sigma[i] 
-        y = y + np.prod(phi[:, 0:(i+1)], axis=1) / normalizer
+        normalizer = normalizer * 2 ** sigma[ascending_sigma[i]] 
+        y = y + np.prod(phi[:, ascending_sigma[0:(i+1)]], axis=1) / normalizer
 
     return y   
 
